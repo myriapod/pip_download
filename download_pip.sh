1c5bb1b367f97f67630dd735255d139053d11e7b
sp <solene.poydenot@gmail.com>
2024-10-30 18:20:28 +0100

    modified:   download_pip.sh

--- download_pip.sh
+++ download_pip.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# Début du script
 echo "--- Script de téléchargement de paquets Python avec PIP ---"
 echo ""
 echo "--- Definition de l'environnement:"
+# Display des versions de python par défaut
 echo "Python par défault: $(which python), $(python -V)"
 
-read -p "> Modification de l'environnement ? (y/n)" modifEnv
-if [[ "$modifEnv" == "y" ]]
+# Si l'installation se fait pour une version d'ANACONDA
+read -p "> Installation pour ANACONDA ? (y/n) " anaconda
+if [ "$anaconda" == "y" ]
 then
-    echo ">> Modification de l'environnement ---"
-    read -p "Chemin vers la version de python à utiliser: " pythonPath
-    export PATH=$pythonPath:$PATH && echo "PATH modifié"
-    echo "Environnement utilisé: $(which python), $(python -V)"
-    pythonVer=$(python -V | cut -d" " -f2)
-    echo "<<"
-    echo ""
+    echo ">> Modification de l'environnement pour ANACONDA ---"
+    read -p "Version d'Anaconda utilisée (par ex: 2024.06)" anacondaVer
+    
+    pythonPath="/home/codes/ANACONDA3/$anacondaVer/bin"
+    if [ -d $pythonPath ]
+    then
+        export PATH=$pythonPath:$PATH && echo "PATH modifié"
+        echo "Environnement utilisé: $(which python), $(python -V)"
+        pythonVer=$(python -V | cut -d" " -f2)
+        echo "<<"
+        echo ""
+    else
+        echo "Le dossier $pythonPath n'existe pas"
+        echo "--- Fermeture du script"
+        exit
+    fi
 else
+    # Si pas de version d'ANACONDA utilisée, 
     echo ""
     read -p "Version de python utilisée: " pythonVer
 fi
@@ -26,10 +39,11 @@ pathDownload=$package$packageVer-python$pythonVer
 
 echo "Téléchargement de $package et des dépendances"
 echo "Les paquets sont dans le dossier: $pathDownload"
+
 mkdir $pathDownload
 cd $pathDownload
 
-if [[ "$packageVer" == "" ]]
+if [ "$packageVer" == "" ]
 then
     echo "Version: Dernière version compatible"
     pip download --python-version $pythonVer --only-binary=:all: $package 
@@ -39,6 +53,40 @@ else
 fi
 
 
-echo "Création de requirements.txt"
-ls | cut -d"-" -f1,2 | sed "s/-/=/" >> requirements.txt
-chmod 777 requirements.txt
+echo "Création de $pathDownload-requirements.txt"
+ls | cut -d"-" -f1,2 | sed "s/-/==/" > ../$pathDownload-requirements.txt
+chmod 777 ../$pathDownload-requirements.txt
+
+#if [ "$anaconda" == "y" ]
+#then
+echo "Création de $pathDownload-installed.txt"
+pip freeze > ../$pathDownload-installed.txt
+
+echo "Création de $pathDownload-dependencies.txt"
+cat ../$pathDownload-requirements.txt | while read line
+do
+    rm -rf ../$pathDownload-dependencies.txt
+    if [ ! "$(cat ../$pathDownload-installed.txt | grep $line)" == "" ]
+    then
+        echo $line >> ../$pathDownload-dependencies.txt
+    fi
+done
+
+read -p "Supprimer les paquets dont la dépendance est déjà résolue ? (y/n) " supprDep
+if [ "$supprDep" == "y" ]
+then
+    cat ../$pathDownload-dependencies.txt | sed "s/==/-/" | while read line
+    do
+        echo "Suppression de $line*" && rm -f $line* 
+    done
+
+fi
+#fi
+
+cd ../
+echo "Compression du dossier $pathDownload"
+tar cvf $pathDownload.tar.gz $pathDownload
+echo "Suppression du dossier $pathDownload"
+rm -rf $pathDownload
+
+echo "--- Fin du script ---"
